e85861f82b059a48405c40536ee3f2245d504024
yanad <yana.danilova1509@gmail.com>
2022-06-09 08:39:00 +0200

    Initial commit

---
+++ spring/src/test/java/de/danilova/java/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.danilova.java.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
